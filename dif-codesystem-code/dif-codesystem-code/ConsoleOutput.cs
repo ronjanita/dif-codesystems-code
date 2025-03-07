3022c5a0d19974d9130d3c4e85f5e50428df0624
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-07 12:22:17 +0100

    sz^tarted with switch/case logic

--- dif-codesystem-code/dif-codesystem-code/ConsoleOutput.cs
+++ dif-codesystem-code/dif-codesystem-code/ConsoleOutput.cs
@@ -15,12 +15,17 @@ namespace dif_codesystem_code
         public static void GetInput()
         {
             Console.WriteLine("Welcome to this encryption program, where you are able to encrypt your number based code, into a new number based code system.");
-            Console.WriteLine("Explanation: Fisrt, you select the number system that your input is based on. Second you select the nu,mber system you want your text to get encrypted to. Latly you enter the number code you want ot encrypt. Press enter and your newly encrypted number code get displayed!");
+            Console.WriteLine("Explanation: First, you select the number system that your input is based on. Second you select the nu,mber system you want your text to get encrypted to. Latly you enter the number code you want ot encrypt. Press enter and your newly encrypted number code get displayed!");
             Console.WriteLine("The available number systems are: binary(B), octal(O), decimal(D) and hexadecimal(H).");
 
             Console.WriteLine("Please enter the start-number system(B, O, D, H):");
-            string UserInputStartNumberSystem = Console.ReadLine();   //if else logik machen? oder mit schleife arbieten? 
+            string UserInputStartNumberSystem = Console.ReadLine();   
+            
+            switch (UserInputStartNumberSystem)
+            {
+                case "B";
 
+            }
             Console.WriteLine("Please enter the end-number system(B, O, D, H):");
             string UserInputEndNumberSystem = Console.ReadLine();       //if else logik machen? oder mit schleife arbieten? 
             Console.WriteLine("Please enter your number based code:");
